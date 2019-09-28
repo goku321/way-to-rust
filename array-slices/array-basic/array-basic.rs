74bb400b808f54a7b5313a4c725716ce1977dd77
Deepak <sah.sslpu@gmail.com>
2019-09-28 12:23:29 +0530

    Array basics

---
+++ array-slices/array-basic/array-basic.rs
@@ -0,0 +1,11 @@
+// use std::mem;
+
+fn main() {
+    let xs: [i32; 5] = [1, 2, 3, 4, 5];
+
+    println!("First element of array is: {}", xs[0]);
+
+    let ys: [i32; 500] = [0; 500];
+
+    println!("First element of ys is: {}", ys[0]);
+}
