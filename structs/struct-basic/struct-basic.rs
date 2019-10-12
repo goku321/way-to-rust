83b96bc311e42289fa4faadbdf89a3caa6789d8d
Deepak <sah.sslpu@gmail.com>
2019-10-13 01:02:06 +0530

    Learned defining method on a struct

--- structs/struct-basic/struct-basic.rs
+++ structs/struct-basic/struct-basic.rs
@@ -3,6 +3,17 @@ struct Person<'a> {
     age: u8,
 }
 
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+}
+
 fn main() {
     let name = "Deepak";
     let age = 25;
@@ -10,4 +21,11 @@ fn main() {
 
     // Print the struct
     println!("{} is {} years old", deepak.name, deepak.age);
+
+    let rect = Rectangle { width: 30, height: 30 };
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        rect.area()
+    );
 }
